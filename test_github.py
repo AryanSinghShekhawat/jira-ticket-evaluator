c5d4e8c1b476d7e343f96b56c898bf218cdfd679
Aryan Singh Shekhawat <87198557+AryanSinghShekhawat@users.noreply.github.com>
2026-06-13 22:54:51 +0530

    updated for correction

--- test_github.py
+++ test_github.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 headers = {"Authorization": f"token {os.getenv('GITHUB_TOKEN')}"}
 response = requests.get("https://api.github.com/user", headers=headers)
-print("✅ GitHub Ready!" if response.status_code == 200 else "❌ Token failed")
+print(" GitHub Ready!" if response.status_code == 200 else " Token failed")
